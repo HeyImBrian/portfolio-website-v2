7e8c1f3436c4baca9952a9b29ddc52a5745ea542
HeyImBrian <78251774+HeyImBrian@users.noreply.github.com>
2022-02-09 10:23:03 -0800

    Made lightboxes into ContentContainer props
    This also put the images in the correct locations.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Button from './components/Button.js'
 import ContentContainer from './components/ContentContainer.js'
 import ProjectContent from './ProjectContent.js'
 import imgDesc from './ImgsAndDesc.js'
-
-
-
 import './App.css';
 import './StarsBG.css';
 import './ButtonsStyle.css';
@@ -24,10 +21,9 @@ function App() {
   const mooloolabaWebsite = new ProjectContent(imgDesc.imagesmooloolabaSite, imgDesc.descsMooloolabaSite, imgDesc.overviewMooloolabaSite, imgDesc.linkMooloolabaSite);
 
 
+  // Used in ContentContainer placement
   const w = window.innerWidth;
   const h = window.innerHeight;
-
-
   const LocXLeft = (multiplier) => (w / 2) - ((w / 30) * multiplier );
   const LocXRight = (multiplier) => (w / 2) + ((w / 30) * multiplier );
   const startLocY = 0;
@@ -35,7 +31,7 @@ function App() {
   const diag = ((w/30) * 3);
 
 
-
+  // Used in the ContentContainers
   function generateRange(max){
     let numList = [];
     for (let i = 1; i < max; i++){
@@ -44,11 +40,6 @@ function App() {
     return numList;
   }
 
-  
-  var appointmentProgramSetup = generateRange(appointmentProgram.getImgs().length);
-  var inventoryProgramSetup = generateRange(inventoryProgram.getImgs().length);
-  var mooloolabaWebsiteSetup = generateRange(mooloolabaWebsite.getImgs().length);
-
 
   return (
     <div className="App">
@@ -56,25 +47,12 @@ function App() {
       <div id="stars2"></div>
       <div id="stars3"></div>
       
-      {/* <link rel="stylesheet" type="text/css" href="./components/lightbox/css/lightbox.css"></link>
-      <script src="./components/lightbox/js/lightbox-plus-jquery.js"></script> */}
 
       <header className="App-header">
         <img src={myName} className="myName" alt="Brian St. Germain" />
 
 
         {lightboxPlusJquery.option({'resizeDuration': 0, 'imageFadeDuration': 300})}
-
-        {/* Lightbox setups */}
-        <a href={appointmentProgram.getImgs()[0]} data-lightbox="appointmentGallery" className="lightbox" data-title={appointmentProgram.getDesc()[0]}> <img className="testImg" src={appointmentProgram.getImgs()[0]}></img></a>
-        {appointmentProgramSetup.map( (index) => (<a href={appointmentProgram.getImgs()[index]} data-lightbox="appointmentGallery" className="lightbox" key={index} data-title={appointmentProgram.getDesc()[index]}></a>))}
-
-        <a href={inventoryProgram.getImgs()[0]} data-lightbox="inventoryGallery" className="lightbox" data-title={inventoryProgram.getDesc()[0]}> <img className="testImg" src={inventoryProgram.getImgs()[0]}></img></a>
-        {inventoryProgramSetup.map( (index) => (<a href={inventoryProgram.getImgs()[index]} data-lightbox="inventoryGallery" className="lightbox" key={index} data-title={inventoryProgram.getDesc()[index]}></a>))}
-
-        <a href={mooloolabaWebsite.getImgs()[0]} data-lightbox="mooloolabaGallery" className="lightbox" data-title={mooloolabaWebsite.getDesc()[0]}> <img className="testImg" src={mooloolabaWebsite.getImgs()[0]}></img></a>
-        {mooloolabaWebsiteSetup.map( (index) => (<a href={mooloolabaWebsite.getImgs()[index]} data-lightbox="mooloolabaGallery" className="lightbox" key={index} data-title={mooloolabaWebsite.getDesc()[index]}></a>))}
-
 
 
         <div className="buttonContainer">
@@ -85,7 +63,7 @@ function App() {
           <Button mainText={"My Links"} hiddenText={"GitHub LinkedIn"} subText={"GitHub, LinkedIn"} />
         </div>
 
-        
+  
         <Line name={"line1"} startLocX={LocXLeft(1)} startLocY={startLocY} endLocX={LocXLeft(1)} endLocY={endLocY * 0.5} diagLocX={LocXLeft(1) - diag} diagLocY={endLocY * 0.5 + diag} /> 
         <Line name={"line2"} startLocX={LocXLeft(2)} startLocY={startLocY} endLocX={LocXLeft(2)} endLocY={endLocY * 1.6} diagLocX={LocXLeft(2) - diag} diagLocY={endLocY * 1.6 + diag} /> 
         <Line name={"line3"} startLocX={LocXRight(0)} startLocY={startLocY} endLocX={LocXRight(0)} endLocY={endLocY * 2.3} diagLocX={LocXRight(0) - diag} diagLocY={endLocY * 2.3 + diag} />
@@ -93,11 +71,11 @@ function App() {
         <Line name={"line5"} startLocX={LocXRight(2)} startLocY={startLocY} endLocX={LocXRight(2)} endLocY={endLocY * 2} diagLocX={LocXRight(2) + diag} diagLocY={endLocY * 2 + diag} /> 
 
 
-        <ContentContainer locX={LocXLeft(1) - diag} locY={endLocY * 0.5 + diag} leftAdjust={true} />
-        <ContentContainer locX={LocXLeft(2) - diag} locY={endLocY * 1.6 + diag} leftAdjust={true} />
-        <ContentContainer locX={LocXRight(0) - diag} locY={endLocY * 2.3 + diag} leftAdjust={true} />
-        <ContentContainer locX={LocXRight(1) + diag} locY={endLocY * 1 + diag} leftAdjust={false} />
-        <ContentContainer locX={LocXRight(2) + diag} locY={endLocY * 2 + diag} leftAdjust={false} />
+        <ContentContainer locX={LocXLeft(1) - diag} locY={endLocY * 0.5 + diag} leftAdjust={true} programObj={appointmentProgram} lightboxName={"resumeGallery"} generateRange={generateRange} />
+        <ContentContainer locX={LocXRight(1) + diag} locY={endLocY * 1 + diag} leftAdjust={false} programObj={appointmentProgram} lightboxName={"appointmentGallery"} generateRange={generateRange} />
+        <ContentContainer locX={LocXLeft(2) - diag} locY={endLocY * 1.6 + diag} leftAdjust={true} programObj={inventoryProgram} lightboxName={"inventoryGallery"} generateRange={generateRange} />
+        <ContentContainer locX={LocXRight(2) + diag} locY={endLocY * 2 + diag} leftAdjust={false} programObj={mooloolabaWebsite} lightboxName={"mooloolabaGallery"} generateRange={generateRange} />
+        <ContentContainer locX={LocXRight(0) - diag} locY={endLocY * 2.3 + diag} leftAdjust={true} programObj={mooloolabaWebsite} lightboxName={"mooloolabaGallery"} generateRange={generateRange} />
 
 
       </header>
--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ContentContainer = ({ locX, locY, leftAdjust }) => {
+const ContentContainer = ({ locX, locY, leftAdjust, programObj, lightboxName, generateRange }) => {
     const contentStyle = {
         position: "absolute",
         top: locY + ((80 * window.innerHeight) / 100), // 80 is used as the offset for the canvas, so it is also used here.
@@ -11,10 +11,14 @@ const ContentContainer = ({ locX, locY, leftAdjust }) => {
         border: window.innerWidth * 0.005 + "px solid white",
       }
 
+      var projectImgLength = generateRange(programObj.getImgs().length);
+
 
   return (
     <div style={contentStyle}>
-        <h2>Cool container</h2>
+        <a href={programObj.getImgs()[0]} data-lightbox={lightboxName} className="lightbox" data-title={programObj.getDesc()[0]}> <img className="testImg" src={programObj.getImgs()[0]}></img></a>
+        {projectImgLength.map( (index) => (<a href={programObj.getImgs()[index]} data-lightbox={lightboxName} className="lightbox" key={index} data-title={programObj.getDesc()[index]}></a>))}
+
     </div>
   );
 }
